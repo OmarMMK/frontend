fdd7b4f5b56f9f46e4b86239a337cfd89933ffa3
OmarMMK <155239347+OmarMMK@users.noreply.github.com>
2025-06-02 16:47:27 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+js
+import { Routes, Route } from 'react-router-dom';
+import UserForm from './components/UserForm';
+import AdminDashboard from './components/AdminDashboard';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/form" element={<UserForm />} />
+      <Route path="/admin" element={<AdminDashboard />} />
+    </Routes>
+  );
+}
+export default App;
